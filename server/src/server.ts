42e79e4c02c517a88d5c2424c60acf6e5b7964ee
Victo Santos Dos Reis <victo.reis987@gmail.com>
2025-07-09 15:19:54 -0300

    initial commit
    versionando os primeiros arquivos do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import { fastify } from 'fastify';
+import { 
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider
+} from 'fastify-type-provider-zod';
+import { fastifyCors } from '@fastify/cors';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: 'http://localhost:5173'
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/health', () => {
+    return 'OK!';
+});
+
+app.listen({ port: env.PORT}).then(() => {
+    console.log(`Port: ${env.PORT}`);
+    console.log('HTTP server running!!!!');
+});
